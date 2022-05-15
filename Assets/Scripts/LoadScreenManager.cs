0b9abc4ca8064a4424b3b586ce5374f9736fa2c0
Mateusz Jaworski <mateusz.seler13@gmail.com>
2022-05-15 21:05:56 +0200

    imported audio/fonts & created load screen scene

---
+++ Assets/Scripts/LoadScreenManager.cs
@@ -0,0 +1,31 @@
+using System.Collections;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LoadScreenManager : MonoBehaviour
+{
+    private GameObject pressKeyText;
+    private float entryAnim = 1.5f; // LogoEntry anim duration time
+    private bool allowToPress = false;
+
+    void Start()
+    {
+        if(!pressKeyText) pressKeyText = GameObject.Find("PressKeyText");
+
+        pressKeyText.SetActive(false);
+
+        StartCoroutine(StartInteraction());
+    }
+
+    private void Update()
+    {
+        if (allowToPress && Input.anyKey)
+            SceneManager.LoadScene("Menu");
+    }
+    IEnumerator StartInteraction()
+    {
+        yield return new WaitForSeconds(entryAnim);
+        pressKeyText.SetActive(true);
+        allowToPress = true;
+    }
+}
