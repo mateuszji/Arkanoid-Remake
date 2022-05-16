d4c6a8fb73b932b5bd3986426d1e0bbdb615d277
Mateusz Jaworski <mateusz.seler13@gmail.com>
2022-05-16 21:12:50 +0200

    implemented PaddleMovement & updated graphics

--- Assets/Scripts/Paddle.cs
+++ Assets/Scripts/Paddle.cs
@@ -4,29 +4,36 @@ using UnityEngine;
 
 public class Paddle : MonoBehaviour
 {
-    [SerializeField]
     private Camera mainCamera;
+    private SpriteRenderer sr;
     private float paddlePosY;
     private float mousePosPixels;
     private float mousePosX;
-    private float leftClamp = 125;
-    private float rightClamp = 415;
+    private float defaultPaddleWithInPixels = 192;
+    private float defaultClamp = 128;
 
     void Start()
     {
-        if (!mainCamera) mainCamera = FindObjectOfType<Camera>();
-        paddlePosY = this.transform.position.y;
+        mainCamera = FindObjectOfType<Camera>();
+        sr = GetComponent<SpriteRenderer>();
+        paddlePosY = transform.position.y;
     }
+
 
     void Update()
     {
+        if (!mainCamera || !sr) return;
+
         movePaddle();
     }
 
     private void movePaddle()
     {
+        float paddleShift = (defaultPaddleWithInPixels - ((defaultPaddleWithInPixels / 2) * sr.size.x)) / 2f;
+        float leftClamp = defaultClamp - paddleShift;
+        float rightClamp = 540 - defaultClamp + paddleShift;
         mousePosPixels = Mathf.Clamp(Input.mousePosition.x, leftClamp, rightClamp);
         mousePosX = mainCamera.ScreenToWorldPoint(new Vector3(mousePosPixels, 0, 0)).x;
-        this.transform.position = new Vector3(mousePosX, paddlePosY, 0);
+        transform.position = new Vector3(mousePosX, paddlePosY, 0);
     }
 }
